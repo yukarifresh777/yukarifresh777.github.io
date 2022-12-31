f3d303cd26c8deee42684ce758e1729b31347d8c
yukarifresh777 <120928053+yukarifresh777@users.noreply.github.com>
2022-12-31 11:25:22 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+`use strict`;
+
+{
+    const btn = document.getElementById(`btn`);
+    const result = document.getElementById(`result`);
+
+    btn.addEventListener(`click`, () => {
+      const results = [`大吉`, `中吉`, `小吉`, `凶`];
+      const n = Math.floor(Math.random() * results.length);
+
+      result.textContent = results[n];
+
+    });
+}
